ab4a6d6f96dbcc61bab359bddf8474eda2ba8562
Panayotis Manganaris <panos.manganaris@gmail.com>
2023-10-29 18:10:27 -0400

    configure backup, autosave, window, session and file recovery

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -41,3 +41,35 @@
 
 (global-set-key (kbd "C-z") 'ignore)
 (global-set-key (kbd "C-x C-z") 'ignore)
+					; failure recovery
+(use-package recentf
+  :init
+  (recentf-mode 1))
+
+(use-package emacs
+  :config
+  (if (not (daemonp))
+      (desktop-save-mode 1)
+    (defun restore-desktop (frame)
+      "Restores desktop and cancels hook after first frame opens.
+     So the daemon can run at startup and it'll still work"
+      (with-selected-frame frame
+	(desktop-save-mode 1)
+	(let ((desktop-load-locked-desktop t))
+          (desktop-read)
+          (remove-hook 'after-make-frame-functions 'restore-desktop))))
+    (add-hook 'after-make-frame-functions 'restore-desktop))
+
+  (winner-mode 1)
+
+  (setq backup-directory-alist
+	'(("." . "~/.local/share/emacs/saves/")))
+  (setq backup-by-copying t
+	;; symlinked files + metadata
+        version-control t
+        delete-old-versions t
+        kept-new-versions 6
+        kept-old-versions 2)
+  (setq auto-save-default t
+        auto-save-timeout 60
+        auto-save-interval 200))
