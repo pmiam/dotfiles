7b1ee229b1396b64dd4b4b4fe3b03c20d44ec6b5
Panayotis Manganaris <panos.manganaris@gmail.com>
2024-01-26 22:43:15 -0500

    nyxt: install and load nx-zotero

--- nyxt/.config/nyxt/config.lisp
+++ nyxt/.config/nyxt/config.lisp
@@ -1,3 +1,12 @@
 (define-configuration buffer
   ((default-modes
     (pushnew 'nyxt/mode/emacs:emacs-mode %slot-value%))))
+
+(nyxt:define-nyxt-user-system-and-load "nyxt-user/nx-zotero-mode-proxy"
+  :description "This proxy system saves us if nx-zotero fails to load.
+Otherwise it will break all the config loading."
+  :depends-on ("nx-zotero-mode"))
+
+(define-configuration web-buffer
+  ((default-modes
+    (pushnew 'zotero-mode %slot-value%))))
