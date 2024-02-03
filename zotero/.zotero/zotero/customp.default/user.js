4bf007694d56d1bd25d699a9fb2466d28bdd20de
Panayotis Manganaris <panos.manganaris@gmail.com>
2024-02-02 22:19:09 -0500

    zotero [ITER] rebase custom export script

--- zotero/.zotero/zotero/customp.default/user.js
+++ zotero/.zotero/zotero/customp.default/user.js
@@ -4,6 +4,10 @@ user_pref("extensions.zotero.translators.better-bibtex.autoExportDelay", 30);
 user_pref("extensions.zotero.translators.better-bibtex.bibtexURL", "url");
 user_pref("extensions.zotero.translators.better-bibtex.citekeyFormat", "auth.lower + year + shorttitle(3,3)");
 user_pref("extensions.zotero.translators.better-bibtex.citekeyFormatEditing", "auth.lower + year + shorttitle(3,3)");
+user_pref(
+    "extensions.zotero.translators.better-bibtex.postscript",
+    ""
+);
 user_pref("extensions.zotero.translators.better-bibtex.quickCopyMode", "selectlink");
 user_pref("extensions.zotero.translators.better-bibtex.skipFields", "abstract");
 user_pref("extensions.zotero.useDataDir", true);
