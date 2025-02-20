3911239b3ad84850b27dd6957547add5c3d18a51
Panayotis Manganaris <panos.manganaris@gmail.com>
2025-02-20 04:17:25 -0500

    chezmoi [ITER] pure bourne shell etcmoi hooks

--- .post_update_apply.sh
+++ .post_update_apply.sh
@@ -1,6 +1,11 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-if ! [[ $CHEZMOI_ARGS =~ /etcmoi ]]; then
+case $CHEZMOI_ARGS in
+    */etcmoi* ) HANDLE_ETCMOI=false;;
+    * ) HANDLE_ETCMOI=true;;
+esac
+
+if $HANDLE_ETCMOI; then
     echo "applying to /etc!"
     $CHEZMOI_ARGS -D /etc -S $CHEZMOI_HOME_DIR/.config/etcmoi
 fi
