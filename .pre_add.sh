8ed20041c3b13e5dc00d9f9361eb15bd33ef1c0a
Panayotis Manganaris <panos.manganaris@gmail.com>
2025-02-20 04:17:25 -0500

    chezmoi [ITER] pure bourne shell etcmoi hooks

--- .pre_add.sh
+++ .pre_add.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
 
-if [[ $CHEZMOI_ARGS =~ /etc ]] & ! [[ $CHEZMOI_ARGS =~ /etcmoi ]]; then
+case $CHEZMOI_ARGS in
+    */etcmoi* ) HANDLE_ETCMOI=false;;
+    */etc/* ) HANDLE_ETCMOI=true;;
+    * ) HANDLE_ETCMOI=false;;
+esac
+
+if $HANDLE_ETCMOI; then
     echo "adding /etc files!"
     $CHEZMOI_ARGS -D /etc -S $CHEZMOI_HOME_DIR/.config/etcmoi
     $CHEZMOI_EXECUTABLE $CHEZMOI_COMMAND $CHEZMOI_HOME_DIR/.config/etcmoi
